37144853090fbd9f8dd8f8e4061cfba750682752
Cade DuPont <cadedupont45@gmail.com>
2024-01-17 17:25:47 -0600

    Add JS files and documentation

---
+++ README.md
@@ -0,0 +1,27 @@
+# Notion Bookshelf
+
+JavaScript application for querying Google Books for a given book title and adding all of the book's information as a page to a Notion database.
+
+## Setting up API keys
+Before attempting to run the program, ensure that you have a Notion database present in your workspace.
+
+You will need to install Node.js on your machine and create a `.env` file in the root directory of the project containing the following:
+
+```
+NOTION_TOKEN=your_key_here
+NOTION_DATABASE_ID=your_key_here
+GOOGLE_BOOKS_API_KEY=your_key_here
+```
+
+Replace every instance of `your_key_here` with the corresponding [Notion](https://developers.notion.com/) and [Google Books](https://developers.google.com/books/docs/overview) API keys. The `NOTION_DATABASE_ID` is a string of letters and numbers identifying the database in the URL. A short guide to finding this string can be found [here](https://stackoverflow.com/a/69860478).
+
+## To Run
+The program is currently configured to run using Node.js. Run the following command at the project's root directory to install dependencies from the `package.json` file:<br>
+
+`npm install`
+
+Then, to run the JavaScript file using the Node.js interpreter, run the following command:<br>
+
+`node main.js '<book-title>'`<br>
+
+Replace `'<book-title>'` with the title of the book you are looking to search for. Make sure to wrap the title in either single quotes or double quotes so the program can parse the command line arguments as intended.
---
+++ main.js
@@ -0,0 +1,152 @@
+require('dotenv').config();
+const { Client } = require('@notionhq/client');
+
+// Load environment variables
+const { NOTION_TOKEN, NOTION_DATABASE_ID, GOOGLE_BOOKS_API_KEY } = process.env;
+
+// If no query is provided, exit with error
+const query = process.argv[2];
+if (!query) {
+    throw new Error('No query provided');
+}
+
+// Form Google Books API URL
+const google_books_url = `https://www.googleapis.com/books/v1/volumes?q=${query.split('+')}&key=${GOOGLE_BOOKS_API_KEY}`
+
+// Create Notion API instance
+const notion = new Client({
+    auth: NOTION_TOKEN,
+});
+
+// Function for gathering each page in the database
+const getPages = async () => {
+    // Create an array to store pages, and a cursor to paginate through results
+    const pages = [];
+    let cursor = undefined;
+
+    // While there are more pages to get, get pages from database
+    while (true) {
+        // Get pages from Notion API
+        const { results, next_cursor } = await notion.databases.query({
+            database_id: NOTION_DATABASE_ID,
+            start_cursor: cursor,
+        });
+
+        // Add pages to array and update cursor
+        pages.push(...results);
+        if (!next_cursor)
+            break;
+        cursor = next_cursor;
+    }
+
+    // Return array of pages
+    return pages;
+};
+
+// Fetch data from Google Books API
+fetch(google_books_url)
+    .then(response => response.json())
+    .then(data => {
+        // Declare book variable, set to first book in data
+        let num = 0;
+        let book = data.items[num].volumeInfo;
+
+        // If any properties are undefined, set book to next book in data until all properties are defined
+        let properties = [undefined];
+        while (properties.includes(undefined)) {
+            book = data.items[num++].volumeInfo;
+            properties = [
+                book.title,
+                book.authors[0],
+                book.publisher,
+                book.pageCount,
+                book.industryIdentifiers[0].identifier,
+                book.description,
+                book.imageLinks.thumbnail
+            ];
+        }
+
+        // If book is not found, exit with error
+        if (!book || book.title.toLowerCase() !== query.toLowerCase()) {
+            throw new Error('Book not found');
+        }
+
+        // Get pages from database
+        getPages().then(pages => {
+            // Find page with matching title
+            const page = pages.find(page => page.properties.Title.title[0].plain_text.toLowerCase() === book.title.toLowerCase());
+
+            // If page exists, exit with error; otherwise, create page
+            if (page !== undefined) {
+                throw new Error('Book already exists in database');
+            } else {
+                // Create book page in database; add title, author, ISBN, cover image, and description
+                notion.pages.create({
+                    cover: {
+                        type: 'external',
+                        external: {
+                            url: book.imageLinks.thumbnail
+                        }
+                    },
+                    parent: {
+                        database_id: NOTION_DATABASE_ID
+                    },
+                    properties: {
+                        Title: {
+                            title: [{
+                                text: {
+                                    content: book.title
+                                }
+                            }]
+                        },
+                        Author: {
+                            rich_text: [{
+                                text: {
+                                    content: book.authors[0]
+                                }
+                            }]
+                        },
+                        Publisher: {
+                            rich_text: [{
+                                text: {
+                                    content: book.publisher
+                                }
+                            }]
+                        },
+                        'Page Count': {
+                            number: book.pageCount
+                        },
+                        ISBN: {
+                            rich_text: [{
+                                text: {
+                                    content: book.industryIdentifiers[0].identifier
+                                }
+                            }]
+                        },
+                    },
+                    children: [
+                        {
+                            object: 'block',
+                            heading_3: {
+                                rich_text: [{
+                                    text: {
+                                        content: 'Description'
+                                    }
+                                }]
+                            }
+                        },
+                        {
+                            object: 'block',
+                            paragraph: {
+                                rich_text: [{
+                                    text: {
+                                        content: book.description,
+                                    }
+                                }],
+                            }
+                        }
+                    ]
+                });
+            }
+        });
+    });
